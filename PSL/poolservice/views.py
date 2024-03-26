bced5b11c57a20ba2f8994b78e53e76ec0551b32
BeastBeastov <7710963@mail.ru>
2024-03-26 17:07:53 +0300

    Revert "fix bags"
    This reverts commit 3e417eadecf88826d9dd5f4bc8c3f26dc02343fa.

--- PSL/poolservice/views.py
+++ PSL/poolservice/views.py
@@ -251,49 +251,12 @@ class PoolView(DataMixin, DetailView):
     model = Pool
     template_name = 'poolservice/pool_show.html'
     slug_url_kwarg = 'pool_slug'
-    current_year = datetime.now().year
-    start_date = datetime(current_year,1,1)
-
-    def get(self, request, pool_slug):
-        context = {
-            'pool': Pool.objects.get(slug=pool_slug),
-            'menu': menu+appmenu
-        }
-        if request.GET.get('start_date'):
-            self.start_date = request.GET.get('start_date')
-        queryset = PoolService.objects.filter(pool=context['pool'])
-        date_book = list()
-        for log in queryset:
-            date_book.append(log.time_create)
-        context['date_book'] = date_book
-        context['logs'] = queryset.filter(time_create__gte=self.start_date)
-        first = 0
-        for log in context['logs']:
-            first = log
-        if first:
-            context['first_log_time'] = first.time_create
-        if context['logs']:
-            context['reagents_book'] = reagent_statistics(context['logs'])[0]
-            context['rs_book'] = reagent_statistics(context['logs'])[1]
-        total = queryset.count()
-        ph_count = queryset.filter(PH__gte=5).count()
-        rx_count = queryset.filter(RX__gte=300).count()
-        cl_count = queryset.filter(CL__gte=0.05).count()
-        t_count = queryset.filter(T__gte=10).count()
-        if ph_count >= total / 2: context['show_ph'] = True
-        if rx_count >= total / 2: context['show_rx'] = True
-        if cl_count >= total / 2: context['show_cl'] = True
-        if t_count >= total / 2: context['show_t'] = True
-        return render(request, 'poolservice/pool_show.html'.format(self.start_date), context=context)
+    start_time = datetime(2023,1,1)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         queryset = PoolService.objects.filter(pool=context['pool'])
-        date_book = list()
-        for log in queryset:
-            date_book.append(log.time_create.date())
-        context['date_book'] = date_book
-        context['logs'] = queryset.filter(time_create__gte=self.start_date)
+        context['logs'] = queryset.filter(time_create__gte=self.start_time)
         first = 0
         for log in context['logs']:
             first = log
