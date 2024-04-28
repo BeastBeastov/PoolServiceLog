834eea932f741994d00408ebe591e55a39ae3929
BeastBeastov <7710963@mail.ru>
2024-04-28 14:31:21 +0300

    time_create requaered bug rest_api

--- PSL/poolservice/serializers.py
+++ PSL/poolservice/serializers.py
@@ -1,8 +1,10 @@
 import io
 
+from django.utils import timezone
 from rest_framework import serializers
 
 from .models import PoolService
+
 
 class PoolServiceModel:
     # def __init__(self, title, pool, time_create, time_update, PH, RX, CL, T, water_cond, reagents, works,comment, is_published, author):
@@ -12,7 +14,9 @@ class PoolServiceModel:
 
 
 class PoolServiceSerializer(serializers.ModelSerializer):
+    time_create = serializers.DateTimeField(required=False)
 
     class Meta:
         model = PoolService
         fields = '__all__'
+
