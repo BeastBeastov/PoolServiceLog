a22b367efe5cedaddc439348069ac9f681464cbb
BeastDescktop <7710963@mail.ru>
2025-03-25 20:56:38 +0300

    stasic_root

--- PSL/PSL/settings.py
+++ PSL/PSL/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
